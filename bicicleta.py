db44dd067d30a51c29c51e065bfcc7f7f80a46f6
rafaelolimpioo <rafamaia1011@gmail.com>
2026-03-18 14:08:54 -0300

    bicicleta2

--- bicicleta.py
+++ bicicleta.py
@@ -6,21 +6,24 @@ class bicicleta:
         self.valor = valor
 
     def buzinar(self):
-        print("plim plim")
+        print("plim plim...")
 
     def parar(self):
         print("parando...")
-        print("bicicleta parada")
+        print("bicicleta parada!!!")
 
     def correr(self):
         print("vrummm...")
 
 b1 = bicicleta("bmx", "preta" , "2020" , "1200")
+print (f"cor: {b1.cor} \nmodelo: {b1.modelo} \nano: {b1.ano} \nvalor: {b1.valor}")
 b1.buzinar()
 b1.correr()
-b1.parar()
+b1.parar() 
+print("\n")
 
-b2 = bicicleta("caloi", "azul" , "2015", "550 \n")
+b2 = bicicleta("caloi", "azul" , "2015", "550")
+print(f"cor: {b2.cor} \nmodelo: {b2.modelo} \nano: {b2.ano} \nvalor: {b2.valor}")
 b2.buzinar()
 b2.correr()
 b2.parar()
